0f315f128b1da65249f2c99c4e2dd59bda08b0ab
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-27 23:36:23 +1000

    Changing function so it accepts data from online site, http://www.bitstorm.org/gameoflife/lexicon/

--- application/file_reader.py
+++ application/file_reader.py
@@ -1,28 +1,16 @@
 import sys
 
-def line_to_int_list(line):
-    int_list = []
-    line_no_spaces = line.rstrip()
-
-    for s in line_no_spaces:
-        try:
-            new_int = int(s)
-            int_list.append(new_int)
-        except ValueError:
-            print('Non-digit in file, exiting')
-            sys.exit()
-
-    return int_list
-
-
 def get_board_data(file_location):
     board_data = []
     
     with open(file_location, 'r') as data_file:
         text_data = data_file.readlines()
-        for line in text_data:
-            int_list = line_to_int_list(line)
-            board_data.append(int_list)
-    
+        for i, line in enumerate(text_data):
+            if i < 2:
+                # .cells files have two lines before the data.
+                continue
+            line_no_endings = line.replace('\n', '')
+            board_data.append(line_no_endings)
+
     return board_data
 
