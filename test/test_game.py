5964965d6de86a8cd50c6328b2b9a74c25929f92
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-28 01:33:55 +1000

    Adding tests for evaluating rules for the game

--- test/test_game.py
+++ test/test_game.py
@@ -55,5 +55,92 @@ class TestCountNeighbours(unittest.TestCase):
         assert board_data[1][1].neighbour_count == 2
 
 
+class TestSquaresEvaluate_1(unittest.TestCase):
+    '''
+    Once neighbours are counted, loop through and evaluate whether each
+    square will live or die.
+    '''
+
+    def test(self):
+        '''
+        Test data looks like
+
+            O.
+            OO
+        
+        '''
+
+        mock_data_from_file = ['..']
+
+        simulation = game.Game(mock_data_from_file)
+
+        s_0_0 = squares.Square(0, 0, True)
+        s_0_1 = squares.Square(0, 1, False)
+        s_1_0 = squares.Square(1, 0, True)
+        s_1_1 = squares.Square(1, 1, True)
+
+        board_data = [
+            [s_0_0, s_0_1],
+            [s_1_0, s_1_1]
+        ]
+
+        simulation.squares_evaluate(board_data)
+
+        assert board_data[0][0].will_be_alive == True
+        assert board_data[0][1].will_be_alive == True
+        assert board_data[1][0].will_be_alive == True
+        assert board_data[1][1].will_be_alive == True
+
+
+class TestSquaresEvaluate_2(unittest.TestCase):
+    '''
+    Once neighbours are counted, loop through and evaluate whether each
+    square will live or die.
+    '''
+
+    def test(self):
+        '''
+        Test data looks like
+
+            .O.
+            O..
+            OOO
+        
+        '''
+
+        mock_data_from_file = ['..']
+
+        simulation = game.Game(mock_data_from_file)
+
+        s_0_0 = squares.Square(0, 0, False)
+        s_0_1 = squares.Square(0, 1, True)
+        s_0_2 = squares.Square(0, 2, False)
+        s_1_0 = squares.Square(1, 0, True)
+        s_1_1 = squares.Square(1, 1, False)
+        s_1_2 = squares.Square(1, 2, False)
+        s_2_0 = squares.Square(2, 0, True)
+        s_2_1 = squares.Square(2, 1, True)
+        s_2_2 = squares.Square(2, 2, True)
+
+        board_data = [
+            [s_0_0, s_0_1, s_0_2],
+            [s_1_0, s_1_1, s_1_2],
+            [s_2_0, s_2_1, s_2_2]
+        ]
+
+        simulation.squares_evaluate(board_data)
+       
+        assert board_data[0][0].will_be_alive == False
+        assert board_data[0][1].will_be_alive == False
+        assert board_data[0][2].will_be_alive == False
+        
+        assert board_data[1][0].will_be_alive == True
+        assert board_data[1][1].will_be_alive == False
+        assert board_data[1][2].will_be_alive == True
+
+        assert board_data[2][0].will_be_alive == True
+        assert board_data[2][1].will_be_alive == True
+        assert board_data[2][2].will_be_alive == False
+
 if __name__ == '__main__':
     unittest.main()
